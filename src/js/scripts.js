8765d4e24c5137bb4117c5e2d7d79286182bdc35
Dominic <dominic.clay@gmail.com>
2024-04-27 16:35:44 +0100

    Add sphere

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
-const fov = 75;
+const fov = 45;
 const perspectiveRatio = window.innerWidth/window.innerHeight;
 const near = 0.1;
 const far = 1000;
@@ -20,17 +20,42 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera,renderer.domElement);
 const axesHelper = new THREE.AxesHelper(3);
+
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({color:0x00FF00}) 
+const boxMaterial = new THREE.MeshBasicMaterial({
+    color:0x00FF00
+}) 
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
+
+const planeGeometry = new THREE.PlaneGeometry(30,30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color:0xFFFFFF,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+const sphereGeometry = new THREE.SphereGeometry(4,50,50);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color:0x0000FF,
+    wireframe: true
+})
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+plane.rotation.x = -0.5 * Math.PI;
+//plane.rotateX(-0.5 * Math.PI);
+const gridHelper = new THREE.GridHelper(30,10)
+
 box.rotation.x = 5;
 box.rotation.y = 5;
-camera.position.z = 5;
-camera.position.y = 2;
-camera.position.x = 0;
+camera.position.z = -10;
+camera.position.y = 30;
+camera.position.x = 30;
 orbit.update();
 scene.add(axesHelper);
 scene.add(box);
+scene.add(sphere);
+scene.add(plane);
+scene.add(gridHelper);
 
 function animate(time){
     box.rotation.x = time / 1000;
